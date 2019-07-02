281f30b5906c8d3b42254f90e0c226c7de93347e
Fernando Seguim <fseguim@meuacesso.com.br>
2019-07-02 00:54:58 -0300

    create create order command handler

---
+++ src/Menyoo.OrderBasket/Domain/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Menyoo.OrderBasket.Domain.Commands;
+using Menyoo.OrderBasket.Domain.Contracts.Handlers.Commands;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Menyoo.OrderBasket.Domain.Contracts.Commands;
+
+namespace Menyoo.OrderBasket.Domain.Handlers
+{
+    public class CreateOrderCommandHandler : ICreateOrderCommandHandler
+    {
+        public Task<CommandResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken) { throw new NotImplementedException(); }
+    }
+}

72488f2374ff6566c72b8e0ca2851eda779aa6b8
Fernando Seguim <fseguim@meuacesso.com.br>
2019-07-02 00:54:38 -0300

    create fail fast validator command behavior

---
+++ src/Menyoo.OrderBasket/Domain/Handlers/Behaviors/FailFastValidationBehavior.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Menyoo.OrderBasket.Domain.Contracts.Commands;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Menyoo.OrderBasket.Domain.Handlers.Behaviors
+{
+    public class FailFastValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : Command
+    where TResponse : CommandResult
+    {
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next) =>
+            request.IsValid() ? next() : Task.FromResult(new CommandResult { Success = false } as TResponse);
+    }
+}

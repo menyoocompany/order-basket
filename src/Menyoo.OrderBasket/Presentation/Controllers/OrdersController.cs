29e46b13ae161073b245f9c181d6691d3eb9c46b
Fernando Seguim <fseguim@meuacesso.com.br>
2019-07-02 00:55:15 -0300

    implement orders controller

---
+++ src/Menyoo.OrderBasket/Presentation/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Menyoo.OrderBasket.Domain.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Menyoo.OrderBasket.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public OrdersController(IMediator mediator) { _mediator = mediator; }
+        
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return result.Success ? (IActionResult) Created("", result.Data) : BadRequest();
+        }
+    }
+}
